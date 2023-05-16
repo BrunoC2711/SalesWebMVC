126e018d05c30ca1f30bdde42902770271ab4828
BrunoC2711 <brunoalbuquerque1998@gmail.com>
2023-05-16 20:54:26 -0300

    Meu projeto em andamento com alguns CRUDs implementados

---
+++ Data/SalesWebMVCContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMVC.Models;
+
+namespace SalesWebMVC.Data
+{
+    public class SalesWebMVCContext : DbContext
+    {
+        public SalesWebMVCContext (DbContextOptions<SalesWebMVCContext> options)
+            : base(options)
+        {
+
+            //Comentar linhas 13 e 14 na aplicação das migrations
+            SeedingService seedingService = new(this);
+            seedingService.Seed();
+
+        }
+        public DbSet<Department> Department { get; set; } = default!;
+        public DbSet<Seller> Seller { get; set; } = default!;
+        public DbSet<SalesRecord> SalesRecord { get; set; } = default!;
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMVC.Data;
+using SalesWebMVC.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+string mySqlConnection = builder.Configuration.GetConnectionString("SalesWebMVCContext");
+builder.Services.AddDbContext<SalesWebMVCContext>(options =>
+    options.UseMySql(mySqlConnection,
+            ServerVersion.AutoDetect(mySqlConnection))); 
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddScoped<SeedingService>();
+builder.Services.AddScoped<SellerService>();
+
+var app = builder.Build();
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment()) {
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
