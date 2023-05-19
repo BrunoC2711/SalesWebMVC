f8d6250327fce33ec9d0870a6462a967a24db9de
BrunoC2711 <brunoalbuquerque1998@gmail.com>
2023-05-18 23:01:53 -0300

    criada a tela de detalhes e edição de vendedor(Seller), além de exceções personalizadas

--- Models/ViewModels/SellerFormViewModel.cs
+++ Models/ViewModels/SellerFormViewModel.cs
@@ -4,6 +4,6 @@ namespace SalesWebMVC.Models.ViewModels
     public class SellerFormViewModel
     {
         public Seller Seller { get; set; }
-        public ICollection<Department> Departments { get; set; }
+        public ICollection<Department>? Departments { get; set; }
     }
 }
--- Services/SellerService.cs
+++ Services/SellerService.cs
@@ -1,6 +1,8 @@
 ﻿
+using Microsoft.EntityFrameworkCore;
 using SalesWebMVC.Data;
 using SalesWebMVC.Models;
+using SalesWebMVC.Services.Exceptions;
 
 namespace SalesWebMVC.Services
 {
@@ -24,7 +26,7 @@ namespace SalesWebMVC.Services
         }
         public Seller FindById(int? id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
         public void Remove(int? id)
         {
@@ -32,5 +34,23 @@ namespace SalesWebMVC.Services
             _context.Seller.Remove(obj);
             _context.SaveChanges();
         }
+        public void Update(Seller obj)
+        {
+            if (!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id not found");
+            }
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                throw new DbConcurrencyException(ex.Message);
+            }
+
+        }
     }
 }
+
